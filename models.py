1b5a70d71c0b84f3cfabe6d6966792190dcc3295
conformist-mw <conformist.mw@gmail.com>
2017-01-20 08:52:41 +0200

    rename base class to base_class

--- models.py
+++ models.py
@@ -1,10 +1,10 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+base_class = declarative_base()
 
 
-class Orders(Base):
+class Orders(base_class):
     __tablename__ = 'orders'
     id = Column(Integer, primary_key=True, autoincrement=False)
     contact_name = Column(String(200))
