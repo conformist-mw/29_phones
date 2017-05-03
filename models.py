16576d281bfa5a7bf6a92dbc71b8ad4e725dd834
conformist-mw <conformist.mw@gmail.com>
2017-05-04 00:32:38 +0300

    simplify model

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from config import DB_URI
+from os import getenv
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, create_engine
@@ -9,22 +9,16 @@ base_class = declarative_base()
 class Orders(base_class):
     __tablename__ = 'orders'
     id = Column(Integer, primary_key=True, autoincrement=False)
-    contact_name = Column(String(200))
     contact_phone = Column(String(100))
-    contact_email = Column(String(150))
-    status = Column(String)
-    created = Column(String)
-    confirmed = Column(String)
-    comment = Column(String)
-    price = Column(String)
+    fmt_phone = Column(String(100))
 
 
 def get_session():
-    engine = create_engine(DB_URI)
+    engine = create_engine(getenv('DB_URI'))
     session_class = sessionmaker(engine)
     return session_class()
 
 
 if __name__ == '__main__':
-    engine = create_engine(DB_URI)
+    engine = create_engine(getenv('DB_URI'))
     base_class.metadata.create_all(engine)
