2ee9c7b0dcd85d231c98f4d50f6936ff2b80a85f
conformist-mw <conformist.mw@gmail.com>
2017-05-04 00:33:10 +0300

    rewrite while run_query to decorator

--- phones.py
+++ phones.py
@@ -1,35 +1,35 @@
 import re
 from sqlalchemy.exc import DBAPIError
-from dump import d_session
-from models import Orders
+from models import get_session, Orders
+match = re.compile(r'\D')
 
 
-def remove_non_digit(string):
-    string = re.sub(r'\D', '', string)
-    return string[-10:] if len(string) > 10 else string
-
-
-def main():
-    def query():
-        for order in d_session.query(Orders):
-            number = remove_non_digit(order.contact_phone)
-            order.fmt_phone = number
-
-    run_query(query)
+def remove_non_digit(raw_number):
+    number = match.sub('', raw_number)
+    return number[-10:] if len(number) > 10 else number
 
 
 def run_query(f, attempts=3):
-    while attempts:
-        attempts -= 1
-        try:
-            return f()
-        except DBAPIError as exc:
-            if attempts > 0 and exc.connection_invalidated:
-                d_session.rollback()
-            else:
-                raise
+    def wrapper():
+        for _ in range(attempts):
+            try:
+                return f()
+            except DBAPIError:
+                continue
+        else:
+            print('why i"m here')
+            session.rollback()
+    return wrapper
+
+
+@run_query
+def query():
+    for order in session.query(Orders):
+        number = remove_non_digit(order.contact_phone)
+        order.fmt_phone = number
 
 
 if __name__ == '__main__':
-    main()
-    d_session.commit()
+    session = get_session()
+    query()
+    session.commit()
